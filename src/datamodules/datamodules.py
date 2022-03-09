4e80f3b7496c9c8959fdef87d0d09b1f78745b50
Florian Erhard <florian.martin.erhard@gmail.com>
2022-03-09 16:14:34 +0100

    Remove rnd generator in random_split

--- src/datamodules/datamodules.py
+++ src/datamodules/datamodules.py
@@ -87,13 +87,13 @@ class BaseDataModule(LightningDataModule):
                 pass
             
             # We have to do this to ensure the same batch size for unlabeled and labeled examples in SSDKL
-            if self.train_ssdkl and self.batch_size == 'None':
+            if self.train_ssdkl:# and self.batch_size == 'None':
                 self.batch_size = n_samples_train_labeled         
             
             self.data_train_labeled, self.data_train_unlabeled, self.data_val, self.data_test = random_split(
                                                 dataset=self.dataset,
                                                 lengths=split_lengths,
-                                                generator=torch.Generator().manual_seed(42),
+                                                #generator=torch.Generator().manual_seed(42),
             )
             params_dict = self._calc_standardization_params()
 
