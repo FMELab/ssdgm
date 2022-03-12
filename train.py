6d3e1d5ac82d34867ef05f893a87d4c07f355933
Florian Erhard <florian.martin.erhard@gmail.com>
2022-03-12 13:58:00 +0100

    Changes train.py to not load .env

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from omegaconf import DictConfig
 
 # load environment variables from `.env` file if it exists
 # recursively searches for `.env` in all folders starting from work dir
-dotenv.load_dotenv(".env", override=True)
+dotenv.load_dotenv(override=True)
 
 
 @hydra.main(config_path="configs/", config_name="train.yaml")
